8666f98530de91f9fa6e0959458ceb9ad23788ef
gisa chris <gisachris24@gmail.com>
2023-08-01 14:56:17 -0700

    Add And Remove Super Class Initialisation Where Needed

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,9 @@
 class Nameable
-  def correct_name(name)
+  def initialize(name)
+    @name = name
+  end
+
+  def correct_name()
     raise NotImplementedError, "method 'correct_name' must be implemented in a subclass"
   end
 end
