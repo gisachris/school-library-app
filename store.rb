377f82dd0ec82f80b88bd155aecdaa92e8dec539
kessie <kayp3477@gmail.com>
2023-08-09 17:28:41 +0000

    Implement data reading from storage

--- store.rb
+++ store.rb
@@ -1,30 +1,3 @@
-# require 'json'
-
-# class StorageProcedures
-#   def write_to_storage(data)
-#     storage_units = ['rentals.json', 'books.json', 'people.json']
-#     data.each_with_index do |single_array, i|
-#       next if single_array.empty?
-
-#       File.exist?("data/#{storage_units[i]}") ? File.read("data/#{storage_units[i]}") : []
-
-#       single_array_hashes = single_array.map do |item|
-#         if item.respond_to?(:to_h_without_related)
-#           item.to_h_without_related
-#         else
-#           item.to_h
-#         end
-#       end
-
-#       File.open("data/#{storage_units[i]}", 'w') do |file|
-#         file.puts JSON.pretty_generate(single_array_hashes)
-#       end
-#     end
-#     puts 'Data stored successfully'
-#   end
-# end
-
-
 require 'json'
 require 'fileutils'
 
@@ -52,5 +25,25 @@ class StorageProcedures
       end
     end
     puts 'Data stored successfully'
+  end
+
+  def read_from_storage
+    data = []
+
+    storage_units = ['rentals.json', 'books.json', 'people.json']
+    directory_path = 'data'
+
+    storage_units.each do |file_name|
+      file_path = File.join(directory_path, file_name)
+
+      if File.exist?(file_path)
+        data_json = File.read(file_path)
+        data << JSON.parse(data_json, symbolize_names: true)
+      else
+        data << []
+      end
+    end
+
+    data
   end
 end
