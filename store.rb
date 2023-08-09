bfe4d552b117ae94341ef78d6497c4c1acf7da1d
kessie <kayp3477@gmail.com>
2023-08-09 00:08:22 +0000

    Restructured storage procedure

--- store.rb
+++ store.rb
@@ -1,12 +1,43 @@
+# require 'json'
+
+# class StorageProcedures
+#   def write_to_storage(data)
+#     storage_units = ['rentals.json', 'books.json', 'people.json']
+#     data.each_with_index do |single_array, i|
+#       next if single_array.empty?
+
+#       File.exist?("data/#{storage_units[i]}") ? File.read("data/#{storage_units[i]}") : []
+
+#       single_array_hashes = single_array.map do |item|
+#         if item.respond_to?(:to_h_without_related)
+#           item.to_h_without_related
+#         else
+#           item.to_h
+#         end
+#       end
+
+#       File.open("data/#{storage_units[i]}", 'w') do |file|
+#         file.puts JSON.pretty_generate(single_array_hashes)
+#       end
+#     end
+#     puts 'Data stored successfully'
+#   end
+# end
+
+
 require 'json'
+require 'fileutils'
 
 class StorageProcedures
   def write_to_storage(data)
+    directory_path = 'data'
+    FileUtils.mkdir_p(directory_path)
+
     storage_units = ['rentals.json', 'books.json', 'people.json']
     data.each_with_index do |single_array, i|
       next if single_array.empty?
 
-      File.exist?("data/#{storage_units[i]}") ? File.read("data/#{storage_units[i]}") : []
+      file_path = File.join(directory_path, storage_units[i])
 
       single_array_hashes = single_array.map do |item|
         if item.respond_to?(:to_h_without_related)
@@ -16,7 +47,7 @@ class StorageProcedures
         end
       end
 
-      File.open("data/#{storage_units[i]}", 'w') do |file|
+      File.open(file_path, 'w') do |file|
         file.puts JSON.pretty_generate(single_array_hashes)
       end
     end
