4c965e977a7456a2bdfcd93a93dd48bcb43c8573
kessie <kayp3477@gmail.com>
2023-08-10 14:35:46 +0000

    Removed unwanted comments

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -1,22 +1,3 @@
-# require_relative '../decorators'
-
-# class MockNameable
-#   def correct_name
-#     'Mock Correct Name'
-#   end
-# end
-
-# RSpec.describe BaseDecorator do
-#   let(:mock_nameable) { MockNameable.new }
-#   subject { described_class.new(mock_nameable) }
-
-#   describe '#correct_name' do
-#     it 'returns the correct name from the decorated object' do
-#       expect(subject.correct_name).to eq('Mock Correct Name')
-#     end
-#   end
-# end
-
 require_relative '../decorators'
 
 class MockNameable
