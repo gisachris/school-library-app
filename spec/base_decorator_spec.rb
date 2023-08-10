626902251afb03e4c3edb097fd691717bd6d985b
kessie <kayp3477@gmail.com>
2023-08-10 15:38:31 +0000

    Fix linter error

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -16,3 +16,14 @@ RSpec.describe BaseDecorator do
     end
   end
 end
+
+# RSpec.describe BaseDecorator do
+#   let(:mock_nameable) { MockNameable.new }
+#   subject { described_class.new(mock_nameable) }
+
+#   describe '#correct_name' do
+#     it 'returns the correct name from the decorated object' do
+#       expect(subject.correct_name).to eq('Mock Correct Name')
+#     end
+#   end
+# end
