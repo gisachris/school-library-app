e4e813f635e8f9eddcebb8017fe98aba46908d7c
gisa chris <gisachris24@gmail.com>
2023-08-10 17:49:46 -0700

    Fix Linter Error

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -1,13 +1,17 @@
 require_relative '../decorators'
 
 class MockNameable
+  def initialize(correct_name)
+    @correct_name = correct_name
+  end
+
   def correct_name
-    'Mock Correct Name'
+    @correct_name
   end
 end
 
 RSpec.describe BaseDecorator do
-  let(:mock_nameable) { MockNameable.new }
+  let(:mock_nameable) { MockNameable.new('Mock Correct Name') }
   subject { described_class.new(mock_nameable) }
 
   describe '#correct_name' do
@@ -16,14 +20,3 @@ RSpec.describe BaseDecorator do
     end
   end
 end
-
-# RSpec.describe BaseDecorator do
-#   let(:mock_nameable) { MockNameable.new }
-#   subject { described_class.new(mock_nameable) }
-
-#   describe '#correct_name' do
-#     it 'returns the correct name from the decorated object' do
-#       expect(subject.correct_name).to eq('Mock Correct Name')
-#     end
-#   end
-# end
